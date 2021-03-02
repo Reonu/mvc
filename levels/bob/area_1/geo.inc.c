8155fb39285834780f78a8561c8c85191f406364
Reonu <danileon95@gmail.com>
2021-03-02 19:59:39 +0000

    level changes

--- levels/bob/area_1/geo.inc.c
+++ levels/bob/area_1/geo.inc.c
@@ -3,7 +3,8 @@
 const GeoLayout bob_area_1_geo[] = {
 	GEO_NODE_START(),
 	GEO_OPEN_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, bob_dl_Cube_mesh),
+		GEO_TRANSLATE_NODE_WITH_DL(1, 28, -1500, -37, bob_dl_Island_mesh_layer_1),
+		GEO_TRANSLATE_NODE_WITH_DL(1, 0, -5186, 0, bob_dl_Water_mesh_layer_1),
 	GEO_CLOSE_NODE(),
 	GEO_RETURN(),
 };
@@ -29,7 +30,7 @@ const GeoLayout bob_area_1[] = {
 				GEO_CLOSE_NODE(),
 			GEO_CLOSE_NODE(),
 		GEO_CLOSE_NODE(),
-		GEO_DISPLAY_LIST(LAYER_OPAQUE, bob_dl_material_revert_render_settings),
+		GEO_DISPLAY_LIST(1, bob_dl_material_revert_render_settings),
 	GEO_CLOSE_NODE(),
 	GEO_END(),
 };
