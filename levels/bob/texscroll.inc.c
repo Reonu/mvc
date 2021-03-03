66716f2f9ea910d940c427412a32e5365dc0c3be
Reonu <danileon95@gmail.com>
2021-03-03 17:11:25 +0000

    level changes

---
+++ levels/bob/texscroll.inc.c
@@ -0,0 +1,25 @@
+void scroll_bob_dl_Water_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 29;
+	int width = 32 * 0x20;
+	int height = 32 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bob_dl_Water_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.10000002384185791 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+
+}
+void scroll_bob() {
+	scroll_bob_dl_Water_mesh_layer_5_vtx_0();
+}
