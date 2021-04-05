8f537c8f2a21b58278c0034975c1b38e45f9366e
Reonu <danileon95@gmail.com>
2021-04-05 23:18:26 +0100

    stuff

--- src/game/behaviors/majora_platform.inc.c
+++ src/game/behaviors/majora_platform.inc.c
@@ -1,15 +1,22 @@
 void bhv_majora_platform_loop(void) {
     u8 type = ((o->oBehParams >> 24) & 0xFF);
     s16 speed = ((o->oBehParams >> 8) & 0xFF);
-    if (type % 2) speed *= -1;
+    if (type & 1) speed = - speed;
     
-    f32 *platformDirPos = (type < 2 ? &o->oPosX : &o->oPosY);
+    f32 *platformDirPos;
+    if (type < 2) {
+        platformDirPos = &o->oPosX;
+    } else if (type < 4) {
+        platformDirPos = &o->oPosY;
+    } else {
+        platformDirPos = &o->oPosZ;
+    }
 
-if ((o->oTimer > 50) && (o->oTimer <= 200)) {
+if ((o->oTimer > 35) && (o->oTimer <= 110)) {
     *platformDirPos += speed;
-} else if ((o->oTimer > 250) && (o->oTimer <= 400)) {
+} else if ((o->oTimer > 135) && (o->oTimer <= 210)) {
     *platformDirPos -= speed;
-} else if (o->oTimer > 400) {
+} else if (o->oTimer > 210) {
     o->oTimer = -1;
 }
     
