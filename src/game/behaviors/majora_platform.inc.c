bb3155bca832c64066f2c70fcd5474ff03c19ee3
Reonu <danileon95@gmail.com>
2021-04-05 22:03:01 +0100

    epic platform displacement 2 by arthur

--- src/game/behaviors/majora_platform.inc.c
+++ src/game/behaviors/majora_platform.inc.c
@@ -4,21 +4,14 @@ void bhv_majora_platform_loop(void) {
     if (type % 2) speed *= -1;
     
     f32 *platformDirPos = (type < 2 ? &o->oPosX : &o->oPosY);
-    f32 *platformDirVel = (type < 2 ? &o->oVelX : &o->oVelY);
 
-    if (o->oTimer <= 50) {
-        *platformDirVel = 0;
-    } else if (o->oTimer <= 200) {
-        *platformDirPos += speed;
-        *platformDirVel = speed;
-    } else if (o->oTimer <= 250) {
-        *platformDirVel = 0;
-    } else if (o->oTimer <= 400) {
-        *platformDirPos -= speed;
-        *platformDirVel = -speed;
-    } else {
-        o->oTimer = -1;
-    }
+if ((o->oTimer > 50) && (o->oTimer <= 200)) {
+    *platformDirPos += speed;
+} else if ((o->oTimer > 250) && (o->oTimer <= 400)) {
+    *platformDirPos -= speed;
+} else if (o->oTimer > 400) {
+    o->oTimer = -1;
+}
     
     cur_obj_scale((((o->oBehParams >> 16) & 0xFF) + 1)/4.f);
 }
