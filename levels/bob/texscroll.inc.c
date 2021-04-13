9473d9052cc6dfa856291185118706529aca59de
Reonu <danileon95@gmail.com>
2021-04-13 15:26:31 +0100

    before multitexture stuff

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -45,23 +45,36 @@ void scroll_bob_dl_LavaPuddle_mesh_layer_1_vtx_3() {
 void scroll_bob_dl_Water_mesh_layer_1_vtx_0() {
 	int i = 0;
 	int count = 25;
-	int width = 128 * 0x20;
+	int width = 64 * 0x20;
 	int height = 64 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
+	static int timeX;
+	float amplitudeX = 1.0;
+	float frequencyX = 0.20000000298023224;
+	float offsetX = 0.30000001192092896;
+	static int currentY = 0;
+	int deltaY;
 	Vtx *vertices = segmented_to_virtual(bob_dl_Water_mesh_layer_1_vtx_0);
 
-	deltaX = (int)(0.30000001192092896 * 0x20) % width;
+	deltaX = (int)(amplitudeX * frequencyX * coss((frequencyX * timeX + offsetX) * (1024 * 16 - 1) / 6.28318530718) * 0x20);
+	deltaY = (int)(0.20000000298023224 * 0x20) % height;
 
 	if (absi(currentX) > width) {
 		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
 	}
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
 
 	for (i = 0; i < count; i++) {
 		vertices[i].n.tc[0] += deltaX;
+		vertices[i].n.tc[1] += deltaY;
 	}
 	currentX += deltaX;
+	timeX += 1;
+	currentY += deltaY;
 
 }
 void scroll_bob() {
