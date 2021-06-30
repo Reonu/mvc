1043593a88dbbc33acb1d45d1d7195284518b796
Reonu <danileon95@gmail.com>
2021-06-30 16:04:28 +0100

    level changes

--- levels/bob/texscroll.inc.h
+++
@@ -1,4 +0,0 @@
-extern void scroll_bob_dl_B_LavaPuddle_mesh_layer_1_vtx_0();
-extern void scroll_bob_dl_B_LavaPuddle_mesh_layer_1_vtx_3();
-extern void scroll_bob_dl_Water_mesh_layer_5_vtx_0();
-extern void scroll_bob();
