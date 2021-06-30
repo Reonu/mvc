a15b92006458ab7762496057311ca8edd25fe677
Reonu <danileon95@gmail.com>
2021-06-30 16:04:38 +0100

    lava doesn't have camera collision

--- src/engine/surface_load.c
+++ src/engine/surface_load.c
@@ -425,6 +425,7 @@ static s32 surf_has_no_cam_collision(s16 surfaceType) {
         case SURFACE_NO_CAM_COLLISION_77: // Unused
         case SURFACE_NO_CAM_COL_VERY_SLIPPERY:
         case SURFACE_SWITCH:
+        case SURFACE_BURNING:
             flags = SURFACE_FLAG_NO_CAM_COLLISION;
             break;
 
