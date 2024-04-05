e9da0f306f51e70a140d6a1732367c12780720ae
Reonu <danileon95@gmail.com>
2024-04-05 12:12:38 +0100

    fix exit course crashing the game

--- include/config/config_menu.h
+++ include/config/config_menu.h
@@ -26,6 +26,6 @@
  * Decides which level, area and warp ID the "Exit Course" option takes you to (has no effect if you disable Exit Course or enable DEATH_ON_EXIT_COURSE).
  * Ensure that the warp exists, or else the game will crash.
  */
-#define EXIT_COURSE_LEVEL LEVEL_CASTLE
+#define EXIT_COURSE_LEVEL LEVEL_BOB
 #define EXIT_COURSE_AREA 0x01
-#define EXIT_COURSE_NODE 0x1F
+#define EXIT_COURSE_NODE 0x03
